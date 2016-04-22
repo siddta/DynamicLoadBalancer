4d49e6a6116c721ce9ecaabbae79240266139e25
Tarique <tarique.nitk@gmail.com>
2016-04-21 21:01:13 -0500

    first commit

---
+++ src/main/java/mp4/Main.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package mp4;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+/**
+ * @author tarique
+ *
+ */
+public class Main extends AbstractHandler
+{
+	String mode="Local";
+	
+    public void handle(String target,
+                       Request baseRequest,
+                       HttpServletRequest request,
+                       HttpServletResponse response) 
+        throws IOException, ServletException
+    {
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+        response.getWriter().println("<h1>Hello World</h1>");
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        Server server = new Server(8081);
+        server.setHandler(new Main());
+ 
+        server.start();
+        server.join();
+        // call boostraop
+    }
+
+	
+}
