63c3eb2cab93234db3d4d5a029885a67a764145d
Edward Xue <edthemaster1@gmail.com>
2016-04-24 17:46:28 -0500

    Remove extra stuff

--- src/main/java/mp4/Main.java
+++ src/main/java/mp4/Main.java
@@ -15,8 +15,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
 
 /**
  * @author tarique
@@ -49,19 +47,9 @@ public class Main extends AbstractHandler
 
     public static void main(String[] args) throws Exception
     {
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.setContextPath("/");
         Server server = new Server(8081);
-        server.setHandler(context);
+        server.setHandler(new Main());
 
-        ServletHolder jerseyServlet = context.addServlet(
-                org.glassfish.jersey.servlet.ServletContainer.class, "/*");
-        jerseyServlet.setInitOrder(0);
-
-        jerseyServlet.setInitParameter(
-                "jersey.config.server.provider.classnames",
-                Calculator.class.getCanonicalName());
-        
         server.start();
 
         A = new double[totalSize];
