48d6497c092f13f324a515eb7b3feb1c2bd8fb50
Tarique <tarique.nitk@gmail.com>
2016-04-25 17:46:13 -0500

    MP4
    MP$

---
+++ MP4/run-remote.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+java -cp "DynamicLoadBalancer-0.0.1-SNAPSHOT-jar-with-dependencies.jar:lib/*" loadbalancer.Main  remote 0.5 0 http://sp16-cs423-s-g07.cs.illinois.edu:8080/requests 8080 http://sp16-cs423-g07.cs.illinois.edu:8080/requests 8080
+
