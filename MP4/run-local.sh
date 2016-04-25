4df5b5e174fcc8e2f17e73ae3fb115b3bec596a1
Tarique <tarique.nitk@gmail.com>
2016-04-25 18:07:06 -0500

    MP4

--- MP4/run-local.sh
+++ MP4/run-local.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -cp "DynamicLoadBalancer-0.0.1-SNAPSHOT-jar-with-dependencies.jar:lib/*" loadbalancer.Main local 0.5 0 http://sp16-cs423-s-g07.cs.illinois.edu:8080/requests 8080 http://sp16-cs423-g07.cs.illinois.edu:8080/requests 8080
+java -Xmx4g -cp "DynamicLoadBalancer-0.0.1-SNAPSHOT-jar-with-dependencies.jar:lib/*" loadbalancer.Main local 0.5 0 http://sp16-cs423-s-g07.cs.illinois.edu:8080/requests 8080 http://sp16-cs423-g07.cs.illinois.edu:8080/requests 8080
 
